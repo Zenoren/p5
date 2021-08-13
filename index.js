c38b11cacd14b024b19b5dcf4b61bb54eee71ed4
Zenoren <arturmoraud@gmail.com>
2021-08-13 14:21:28 +0200

    modifs x2

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-/* DÉFINITION D'UNE CLASSE SERVANT DE MODÈLE */
+/* Définition d'un modèle pour un article furniture */
 class Furniture {
-	constructor(jsonCameras) {
-		/* Utilisation de la méthode Object.assign pour copier facilement les valeurs des propriétés */
-		jsonCameras && Object.assign(this, jsonCameras); 
+	constructor(jsonFurniture) {
+		jsonFurniture && Object.assign(this, jsonFurniture);
 	}
 	/* méthode pour mettre les prix en euros */
-	getFormatedPrice(cameras) {
+	getFormatedPrice(furniture) {
 		return this.price / 100;
 	}
 }
-/* RÉCUPÉRATION DES DONNÉES ET AFFICHAGE DES ARTICLES */
-fetch("http://localhost:3000/api/cameras") /* Envoi de la requête au service Web */
-	.then(data => data.json()) /* Conversion de la réponse à la requête au format json */
-	.then(jsonListCameras => { /* Récupération de la nouvelle promesse et des données exploitables */
-		for (let jsonCameras of jsonListCameras) { /* Boucle créant de manière dynamique un nouvel objet basé sur le modèle pour chaque article */
-			let Cameras = new Cameras(jsonCameras);
-			/* Sélection de l'emplacement où intégrer l'objet dans la page html et injection du code html */
-			document.querySelector("#cameras").innerHTML += `<div class="col-lg-4 col-xxl-3">
+/* Appel de l'API, affichage de l'ensemble des articles */
+fetch("http://localhost:3000/api/furniture")
+	.then(data => data.json()) /* fonction récupérant la promesse et les données, formatées en json */
+	.then(jsonListFurniture => { /* récupération de la nouvelle promesse et des données exploitables */
+		for (let jsonFurniture of jsonListFurniture) { /* boucle créant de manière dynamique un nouvel objet basé sur le modèle pour chaque article */
+			let furniture = new Furniture(jsonFurniture);
+			/* sélection de l'emplacement où intégrer l'objet dans la page html et injection du code html */
+			document.querySelector("#furniture").innerHTML += `<div class="col-lg-4 col-xxl-3">
                                                                     <div class="card bg-light text-center mb-4">
-                                                                        <img src="${cameras.imageUrl}" alt="image" class="card-img-top" />
+                                                                        <img src="${furniture.imageUrl}" alt="image" class="card-img-top" />
                                                                             <div class="card-body">
-                                                                                <h5 class="card-title">${cameras.name}</h5>
-                                                                                <p class="card-text lead">${cameras.description}</p>
-                                                                                <p class="card-text fw-bold">${cameras.getFormatedPrice()} €</p>
-                                                                                <a href="produit.html?id=${cameras._id}" class="btn btn-primary stretched-link">Détails</a>
+                                                                                <h5 class="card-title">${furniture.name}</h5>
+                                                                                <p class="card-text lead">${furniture.description}</p>
+                                                                                <p class="card-text fw-bold">${furniture.getFormatedPrice()} €</p>
+                                                                                <a href="produit.html?id=${furniture._id}" class="btn btn-primary stretched-link">Détails</a>
                                                                             </div>
                                                                     </div>
                                                                 </div>`;
 		}
 	})
-	/* En cas de défaillance de l'API */
+	/* gestion des erreurs en cas de problème avec le serveur */
 	.catch(function (erreur) {
 		console.log(erreur);
 	});
